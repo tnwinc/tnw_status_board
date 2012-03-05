a94be81d6ae8c7e6bec06fb6afc9fa09238b661c
Ricardo Diaz <github@rickdiaz.com>
2012-03-05 14:19:33 -0500

    added pivotal biggify here, where it should be

---
+++ pivotal-biggify.user.js
@@ -0,0 +1,52 @@
+// ==UserScript==
+// @match https://*.pivotaltracker.com/*
+// ==/UserScript==
+
+setInterval(function() {
+	document.getElementById('header').style.display = 'none';
+	document.getElementById('controlPanel').style.display = 'none';
+
+
+	var alterStuff = function (classToAlter, styleToChange) {
+		var els = document.getElementsByClassName(classToAlter);
+		var elsArray = Array.prototype.slice.call(els, 0);
+		elsArray.forEach(function(el) {
+			styleToChange(el);
+		});
+	}
+
+	var hideStuff = function (classToHide) {
+		alterStuff(classToHide, function(el) {
+			el.style.display = 'none';
+		})
+	}
+
+
+	hideStuff('toggleExpandedButton');
+	hideStuff('storySelector');
+	hideStuff('panelHeader');
+	hideStuff('panelHeaderLeft');
+	hideStuff('panelHeaderRight');
+	hideStuff('storyLabels');
+	hideStuff('flyoverIcon');
+	hideStuff('accepted');
+	alterStuff('storyPreviewText', function(el) { 
+			el.style.fontSize = '24px';
+			el.style.marginTop = '5px';
+			el.style.marginBottom = '5px';
+		})
+	alterStuff('estimate', function(el) { 
+			el.style.fontSize = '24px';
+		})
+	alterStuff('inner', function(el) { 
+			el.style.overflow = 'hidden';
+		})
+	alterStuff('layout', function(el) { 
+			el.style.marginTop = '5px';
+			el.style.borderSpacing = '0';
+		})
+	alterStuff('iterationHeader', function(el) { 
+			el.style.height = '40px';
+			el.style.fontSize = '24px';
+		})
+}, 500)
