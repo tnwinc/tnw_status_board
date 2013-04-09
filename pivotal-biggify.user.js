c522830ee0a18aaea6a6a49db62c52fdd1e05219
Ricardo Diaz <github@rickdiaz.com>
2013-04-09 17:08:35 -0300

    Make sure we're on pivotal site before we run

--- pivotal-biggify.user.js
+++ pivotal-biggify.user.js
@@ -1,63 +1,65 @@
 // ==UserScript==
-// @match https://*.pivotaltracker.com/*
+// @name Pivotal Biggify
+// @description Makes Pivotal usable on a big board
+// @include https://*.pivotaltracker.com/*
 // ==/UserScript==
-
-setInterval(function() {
-	document.getElementById('header').style.display = 'none';
-	document.getElementById('controlPanel').style.display = 'none';
-
-
-	var alterStuff = function (classToAlter, styleToChange) {
-		var els = document.getElementsByClassName(classToAlter);
-		var elsArray = Array.prototype.slice.call(els, 0);
-		elsArray.forEach(function(el) {
-			styleToChange(el);
-		});
-	}
-
-	var hideStuff = function (classToHide) {
-		alterStuff(classToHide, function(el) {
-			el.style.display = 'none';
-		})
-	}
-
-
-	hideStuff('toggleExpandedButton');
-	hideStuff('storySelector');
-	hideStuff('panelHeader');
-	hideStuff('panelHeaderLeft');
-	hideStuff('panelHeaderRight');
-	hideStuff('storyLabels');
-	hideStuff('flyoverIcon');
-	hideStuff('accepted');
-	alterStuff('storyPreviewText', function(el) { 
-		el.style.fontSize = '28px';
-		el.style.marginTop = '5px';
-		el.style.marginBottom = '5px';
-		el.style.lineHeight = '.9em';
-	})
-	alterStuff('estimate', function(el) { 
-		el.style.fontSize = '28px';
-	})
-	alterStuff('inner', function(el) { 
-		el.style.overflow = 'hidden';
-	})
-	alterStuff('layout', function(el) { 
-		el.style.marginTop = '5px';
-		el.style.borderSpacing = '0';
-	})
-	alterStuff('iterationHeader', function(el) { 
-		el.style.height = '40px';
-	})
-        alterStuff('date', function(el) {
-        	el.style.fontSize = '28px';
-        })
-        alterStuff('iteration_length', function(el) {
-        	el.style.fontSize = '28px';
-        })
-        alterStuff('points', function(el) {
-        	el.style.fontSize = '28px';
-        })
-	var backlogList = document.getElementById("backlog_itemList");
-	backlogList.style.height = 'auto';
-}, 500)
+ 
+if (location.hostname == 'www.pivotaltracker.com') {
+  setInterval(function() {
+    document.getElementById('header').style.display = 'none';
+    document.getElementById('controlPanel').style.display = 'none';
+ 
+    var alterStuff = function (classToAlter, styleToChange) {
+      var els = document.getElementsByClassName(classToAlter);
+      var elsArray = Array.prototype.slice.call(els, 0);
+      elsArray.forEach(function(el) {
+        styleToChange(el);
+      });
+    }
+ 
+    var hideStuff = function (classToHide) {
+      alterStuff(classToHide, function(el) {
+        el.style.display = 'none';
+      })
+    }
+ 
+    hideStuff('toggleExpandedButton');
+    hideStuff('storySelector');
+    hideStuff('panelHeader');
+    hideStuff('panelHeaderLeft');
+    hideStuff('panelHeaderRight');
+    hideStuff('storyLabels');
+    hideStuff('flyoverIcon');
+    hideStuff('accepted');
+    alterStuff('storyPreviewText', function(el) { 
+      el.style.fontSize = '28px';
+      el.style.marginTop = '5px';
+      el.style.marginBottom = '5px';
+      el.style.lineHeight = '.9em';
+    })
+    alterStuff('estimate', function(el) { 
+      el.style.fontSize = '28px';
+    })
+    alterStuff('inner', function(el) { 
+      el.style.overflow = 'hidden';
+    })
+    alterStuff('layout', function(el) { 
+      el.style.marginTop = '5px';
+      el.style.borderSpacing = '0';
+    })
+    alterStuff('iterationHeader', function(el) { 
+      el.style.height = '40px';
+    })
+    alterStuff('date', function(el) {
+      el.style.fontSize = '28px';
+    })
+    alterStuff('iteration_length', function(el) {
+      el.style.fontSize = '28px';
+    })
+    alterStuff('points', function(el) {
+      el.style.fontSize = '28px';
+    })
+    var backlogList = document.getElementById("backlog_itemList");
+    backlogList.style.height = 'auto';
+  }, 500)
+}
