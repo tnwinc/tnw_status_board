47d4a8cfbdaee04abc68e4a2d028e07f0f83f3da
Chris Breiding <chrisbreiding@gmail.com>
2014-01-15 08:37:47 -0500

    get template precompilation working

--- pivotal/build/templates.js
+++ pivotal/build/templates.js
@@ -1,21 +1,46 @@
-Ember.TEMPLATES["application"] = Ember.Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
-  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
+Ember.TEMPLATES["application"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
+  var buffer = '', stack1, stack2, hashTypes, hashContexts, options, self=this, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression;
 
+function program1(depth0,data) {
+  
+  
+  data.buffer.push("Home");
+  }
 
-  buffer += "<h1>Pivotal Tracker</h1>\n\n";
-  if (helper = helpers.outlet) { stack1 = helper.call(depth0, {hash:{},data:data}); }
-  else { helper = (depth0 && depth0.outlet); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
-  buffer += escapeExpression(stack1)
-    + "\n";
+function program3(depth0,data) {
+  
+  
+  data.buffer.push("Log In");
+  }
+
+  data.buffer.push("<h1>Pivotal Tracker</h1>\n\n<p>");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(1, program1, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || (depth0 && depth0['link-to'])),stack1 ? stack1.call(depth0, "index", options) : helperMissing.call(depth0, "link-to", "index", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</p>\n<p>");
+  hashTypes = {};
+  hashContexts = {};
+  options = {hash:{},inverse:self.noop,fn:self.program(3, program3, data),contexts:[depth0],types:["STRING"],hashContexts:hashContexts,hashTypes:hashTypes,data:data};
+  stack2 = ((stack1 = helpers['link-to'] || (depth0 && depth0['link-to'])),stack1 ? stack1.call(depth0, "login", options) : helperMissing.call(depth0, "link-to", "login", options));
+  if(stack2 || stack2 === 0) { data.buffer.push(stack2); }
+  data.buffer.push("</p>\n\n");
+  hashTypes = {};
+  hashContexts = {};
+  data.buffer.push(escapeExpression(helpers._triageMustache.call(depth0, "outlet", {hash:{},contexts:[depth0],types:["ID"],hashContexts:hashContexts,hashTypes:hashTypes,data:data})));
+  data.buffer.push("\n");
   return buffer;
-  });
-Ember.TEMPLATES["login"] = Ember.Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  
+});
+Ember.TEMPLATES["login"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
+this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
   
 
 
-  return "<h1>Please Log In</h1>\n";
-  });
+  data.buffer.push("<h1>Please Log In</h1>\n");
+  
+});
--- pivotal/gulp/gulp-ember-handlebars.coffee
+++ pivotal/gulp/gulp-ember-handlebars.coffee
@@ -1,17 +1,11 @@
 es = require 'event-stream'
 path = require 'path'
-Handlebars = require 'handlebars'
+emberPrecompile = require 'ember-precompile'
 
 module.exports = ->
   es.map (file, callback)->
-    try
-      compiled = Handlebars.precompile file.contents.toString(), {}
-    catch err
-      return callback err, file
-
     fileName = path.basename(file.path, path.extname(file.path))
-    templateName = fileName.replace(/\.(handlebars|hbs)$/, '').replace(/\./g, '/')
-    compiled = 'Ember.TEMPLATES["' + templateName + '"] = Ember.Handlebars.template(' + compiled + ');'
+    compiled = emberPrecompile file.path, {}
     file.path = path.join path.dirname(file.path), "#{fileName}.js"
     file.contents = new Buffer compiled
     callback null, file
