31c015809fa42b36d52320de697eccaee429b6f1
Chris Breiding <chrisbreiding@gmail.com>
2014-06-12 14:19:44 -0400

    make colon blink

--- static/clock.js
+++ static/clock.js
@@ -1,24 +1,24 @@
 (function () {
 
-  var format = function (units) {
-    return '<span class="day-short">' + units.dayShort + ' </span>' +
-      '<span class="day-full">' + units.dayFull + ' </span>' +
-      '<span class="date-short">' + units.dateShort + ' </span>' +
-      '<span class="date-full">' + units.dateFull + ' </span>' +
-      '<span class="time">' + units.time + '</span>' +
-      '<span class="ampm"> ' + units.ampm + '</span>';
-  };
+  var $ = document.getElementById.bind(document),
+      dayShort = $('day-short'),
+      dayFull = $('day-full'),
+      dateShort = $('date-short'),
+      dateFull = $('date-full'),
+      hours = $('hours'),
+      minutes = $('minutes'),
+      ampm = $('ampm');
 
   setInterval(function () {
     var now = moment();
-    document.getElementById('clock').innerHTML = format({
-      dayShort: now.format('ddd,'),
-      dayFull: now.format('dddd,'),
-      dateShort: now.format('MMM D'),
-      dateFull: now.format('MMMM Do'),
-      time: now.format('h:mm'),
-      ampm: now.format('a')
-    });
+
+    dayShort.innerHTML = now.format('ddd,');
+    dayFull.innerHTML = now.format('dddd,');
+    dateShort.innerHTML = now.format('MMM D');
+    dateFull.innerHTML = now.format('MMMM Do');
+    hours.innerHTML = now.format('h');
+    minutes.innerHTML = now.format('mm');
+    ampm.innerHTML = now.format('a');
   }, 1000);
 
 }());
