1c7b6079211bc7c651bbedb5717c53d3186d2568
Chris Breiding <chrisbreiding@gmail.com>
2014-08-27 15:02:05 -0400

    enable customizing font size and datetime format in url; remove media queries

--- static/clock.js
+++ static/clock.js
@@ -1,31 +1,28 @@
 (function () {
 
-  ['color', 'background'].forEach(function (style) {
-    var match = location.href.match(new RegExp(style + '=([^&]+)'));
+  var getParamValue = function (key) {
+    var match = location.href.match(new RegExp(key + '=([^&]+)'));
     if (match && match[1]) {
-      document.body.style[style] = match[1];
+      return decodeURIComponent(match[1]);
+    }
+  };
+
+  var format = getParamValue('format') || 'ddd, MMM Do {{h:mm}} a';
+
+  ['color', 'background', 'fontSize'].forEach(function (style) {
+    var value = getParamValue(style);
+    if (value) {
+      document.body.style[style] = value;
     }
   });
 
-  var $ = document.getElementById.bind(document),
-      dayShort = $('day-short'),
-      dayFull = $('day-full'),
-      dateShort = $('date-short'),
-      dateFull = $('date-full'),
-      hours = $('hours'),
-      minutes = $('minutes'),
-      ampm = $('ampm');
+  var el = document.getElementById('clock');
 
   setInterval(function () {
-    var now = moment();
-
-    dayShort.innerHTML = now.format('ddd,');
-    dayFull.innerHTML = now.format('dddd,');
-    dateShort.innerHTML = now.format('MMM D');
-    dateFull.innerHTML = now.format('MMMM Do');
-    hours.innerHTML = now.format('h');
-    minutes.innerHTML = now.format('mm');
-    ampm.innerHTML = now.format('a');
+    var dateTime = moment().format(format);
+    dateTime = dateTime.replace('{{', '</span><em>');
+    dateTime = dateTime.replace('}}', '</em><span>');
+    el.innerHTML = '<span>' + dateTime + '</span>';
   }, 1000);
 
 }());
